47aaef6f073676f8e5734608a272d1c93d295d5f
Luciano Paranhos <luciano@paranhos.com.br>
2025-03-23 19:24:21 -0300

    feat: adicionar estrutura inicial

---
+++ ofx-viewer/ofx_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ofx_viewerApp.swift
+//  ofx-viewer
+//
+//  Created by Luciano Paranhos on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ofx_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
