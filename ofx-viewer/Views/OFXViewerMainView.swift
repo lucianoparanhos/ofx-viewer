23f8f7bf90289f2fd7300ff1ce246cb0582e2a73
Luciano Paranhos <luciano@paranhos.com.br>
2025-03-23 21:42:12 -0300

    refactor: mover função de redimensionamento para WindowUtils.swift
    A função animateWindowResize(to:) foi movida da view principal para um
    arquivo separado dentro da pasta Utils, com o objetivo de organizar melhor
    a lógica de janela fora da interface declarativa.

--- ofx-viewer/Views/OFXViewerMainView.swift
+++ ofx-viewer/Views/OFXViewerMainView.swift
@@ -38,27 +38,6 @@ struct OFXViewerMainView: View {
     }
 }
 
-
-
-@MainActor
-func animateWindowResize(to newSize: NSSize) {
-    if let window = NSApp.mainWindow {
-        let currentFrame = window.frame
-        let newOrigin = NSPoint(
-            x: currentFrame.origin.x + (currentFrame.size.width - newSize.width) / 2,
-            y: currentFrame.origin.y + (currentFrame.size.height - newSize.height) / 2
-        )
-        
-        let newFrame = NSRect(origin: newOrigin, size: newSize)
-        
-        NSAnimationContext.runAnimationGroup({ context in
-            context.duration = 0.3 // Tempo da animação em segundos
-            context.allowsImplicitAnimation = true
-            window.setFrame(newFrame, display: true, animate: true)
-        }, completionHandler: nil)
-    }
-}
-
 struct VisualEffectView: NSViewRepresentable {
     let material: NSVisualEffectView.Material
     
